c1f4f52b59466f6136508bd26e485974c32ef243
Mehdi Shahdoost <m.shahdoost@mydigipay.com>
2020-01-26 22:45:24 +0330

    Base code for asellion product application

---
+++ product-app/src/test/java/com/asellion/product/productapp/ProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asellion.product.productapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
